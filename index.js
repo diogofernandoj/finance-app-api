0bfe8f6a4959980ad36262ef6a8e7f2d1b61e19b
Diogo Jorge <diogofernandoj@gmail.com>
2023-09-18 21:34:52 -0300

    chore: add pg and dotenv

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const name = "";
+import "dotenv/config.js";
+import express from "express";
+import { PostgresClient } from "./src/db/postgres/client.js";
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  const results = await PostgresClient.query("SELECT * from users;");
+
+  res.send(results);
+});
+
+app.listen(3000, () => console.log("Listening on port 3000!!!"));
