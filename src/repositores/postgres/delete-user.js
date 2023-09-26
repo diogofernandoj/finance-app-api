d40631b7597d9d5dab11a75d1338cbb6f7dbcfe3
Diogo Jorge <diogofernandoj@gmail.com>
2023-09-26 09:47:46 -0300

    feat: add PostgresDeleteUserRepository

---
+++ src/repositores/postgres/delete-user.js
@@ -0,0 +1,14 @@
+import { PostgresClient } from "../../db/postgres/client.js";
+
+export class PostgresDeleteUserRepository {
+  async execute(userId) {
+    const deletedUser = await PostgresClient.query(
+      `DELETE FROM users
+      WHERE id = $1
+      RETURNING *`,
+      [userId],
+    );
+
+    return deletedUser[0];
+  }
+}
