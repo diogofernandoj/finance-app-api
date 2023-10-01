4c3291a4b43cf10956d052dbf1c4784ad1ca1307
Diogo Jorge <diogofernandoj@gmail.com>
2023-10-01 08:52:02 -0300

    feat: add CreateTransactionUseCase

---
+++ src/use-cases/transaction/create-transaction.js
@@ -0,0 +1,29 @@
+import { v4 as uuidv4 } from "uuid";
+
+import { UserNotFoundError } from "../../errors/user";
+
+export class CreateTransactionUseCase {
+  constructor(createTransactionRepository, getUserById) {
+    this.createTransactionRepository = createTransactionRepository;
+    this.getUserById = getUserById;
+  }
+
+  async execute(createTransactionParams) {
+    const userId = createTransactionParams.id;
+
+    const user = await this.getUserById.execute(userId);
+
+    if (!user) {
+      throw new UserNotFoundError(userId);
+    }
+
+    const transactionId = uuidv4();
+
+    const transaction = await this.createTransactionRepository.execute({
+      ...createTransactionParams,
+      id: transactionId,
+    });
+
+    return transaction;
+  }
+}
