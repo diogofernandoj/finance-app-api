5378cd1b0b84b407769121305784fee86770bee1
Diogo Jorge <diogofernandoj@gmail.com>
2024-04-01 06:27:09 -0300

    feat: add prisma with initial model user

---
+++ prisma/migrations/20240401090847_initial_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "firstName" VARCHAR(50) NOT NULL,
+    "lastName" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
+    "password" VARCHAR(100) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
