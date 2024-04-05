9485990350bff2d39e61ab5b111b9dc01b0c3981
Diogo Jorge <diogofernandoj@gmail.com>
2024-04-05 20:37:13 -0300

    feat: ensure Create and DeleteTransactionRepository use Prisma

--- src/repositories/postgres/transaction/create-transaction.js
+++ src/repositories/postgres/transaction/create-transaction.js
@@ -1,21 +1,16 @@
-import { PostgresClient } from "../../../db/postgres/client.js";
+import { prisma } from "../../../../prisma/prisma.js";
 
 export class PostgresCreateTransactionRepository {
   async execute(createTransactionParams) {
-    const createdTransaction = await PostgresClient.query(
-      `INSERT INTO transactions (id, user_id, title, date, amount, type) 
-      VALUES ($1, $2, $3, $4, $5, $6) 
-      RETURNING *`,
-      [
-        createTransactionParams.id,
-        createTransactionParams.user_id,
-        createTransactionParams.title,
-        createTransactionParams.date,
-        createTransactionParams.amount,
-        createTransactionParams.type,
-      ],
-    );
-
-    return createdTransaction[0];
+    return await prisma.transaction.create({
+      data: {
+        id: createTransactionParams.id,
+        user_id: createTransactionParams.user_id,
+        title: createTransactionParams.title,
+        date: createTransactionParams.date,
+        amount: createTransactionParams.amount,
+        type: createTransactionParams.type,
+      },
+    });
   }
 }
--- src/repositories/postgres/transaction/delete-transaction.js
+++ src/repositories/postgres/transaction/delete-transaction.js
@@ -1,14 +1,15 @@
-import { PostgresClient } from "../../../db/postgres/client.js";
+import { prisma } from "../../../../prisma/prisma.js";
 
 export class PostgresDeleteTransactionRepository {
   async execute(transactionId) {
-    const deletedTransaction = await PostgresClient.query(
-      `DELETE FROM transactions 
-      WHERE id = $1 
-      RETURNING *`,
-      [transactionId],
-    );
-
-    return deletedTransaction[0];
+    try {
+      await prisma.transaction.delete({
+        where: {
+          id: transactionId,
+        },
+      });
+    } catch (error) {
+      return null;
+    }
   }
 }
