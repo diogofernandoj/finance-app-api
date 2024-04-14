515630672b9134370652dc823a6ae5de88d02702
Diogo Jorge <diogofernandoj@gmail.com>
2024-04-14 10:19:29 -0300

    test: ensure 400 is returned when first_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -28,4 +28,24 @@ describe("Create User Controller", () => {
     expect(res.statusCode).toBe(201);
     expect(res.body).toBe(httpRequest.body);
   });
+
+  it("should return 400 if first_name is not provided", async () => {
+    // arrange
+    const createUserUseCase = new CreateUserUseCaseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+
+    const httpRequest = {
+      body: {
+        lastName: "Jorge",
+        email: "jorge@email.com",
+        password: "12345678",
+      },
+    };
+
+    // act
+    const res = await createUserController.execute(httpRequest);
+
+    // assert
+    expect(res.statusCode).toBe(400);
+  });
 });
