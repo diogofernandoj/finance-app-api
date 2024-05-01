621b28c437393afcd211c66a2fc50d8efda5f6a0
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-01 10:40:19 -0300

    test: ensure DeleteUserController returns 200 on deleting user

---
+++ src/controllers/user/delete-user.test.js
@@ -0,0 +1,40 @@
+import { faker } from "@faker-js/faker";
+import { DeleteUserController } from "../index.js";
+
+describe("Delete User Controller", () => {
+  class DeleteUserUseCaseStub {
+    execute() {
+      return {
+        id: faker.string.uuid(),
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 7 }),
+      };
+    }
+  }
+
+  const makeSut = () => {
+    const deleteUserUseCase = new DeleteUserUseCaseStub();
+    const sut = new DeleteUserController(deleteUserUseCase);
+
+    return { deleteUserUseCase, sut };
+  };
+
+  const httpRequest = {
+    params: {
+      userId: faker.string.uuid(),
+    },
+  };
+
+  it("should return 200 on deleting user successfully", async () => {
+    // arrange
+    const { sut } = makeSut();
+
+    // act
+    const res = await sut.execute(httpRequest);
+
+    // assert
+    expect(res.statusCode).toBe(200);
+  });
+});
