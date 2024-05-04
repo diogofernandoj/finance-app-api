fcd0cf0450c5f73df17868d620936ddbe4ac7760
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-04 10:10:05 -0300

    test: ensure DeleteTransactionController returns 200 on deleting a transaction successfully

---
+++ src/controllers/transaction/delete-transaction.test.js
@@ -0,0 +1,42 @@
+import { faker } from "@faker-js/faker";
+import { DeleteTransactionController } from "../index.js";
+import { TransactionType } from "@prisma/client";
+
+describe("DeleteTransactionController", () => {
+  class DeleteTransactionUseCaseStub {
+    async execute() {
+      return {
+        id: faker.string.uuid(),
+        user_id: faker.string.uuid(),
+        title: faker.string.alpha(10),
+        date: faker.date.anytime().toISOString(),
+        amount: faker.number.float(),
+        type: faker.helpers.enumValue(TransactionType),
+      };
+    }
+  }
+
+  const makeSut = () => {
+    const deleteTransactionUseCase = new DeleteTransactionUseCaseStub();
+    const sut = new DeleteTransactionController(deleteTransactionUseCase);
+
+    return { sut, deleteTransactionUseCase };
+  };
+
+  const httpRequest = {
+    params: {
+      transactionId: faker.string.uuid(),
+    },
+  };
+
+  it("should return 200 on deleting a transaction successfully", async () => {
+    // arrange
+    const { sut } = makeSut();
+
+    // act
+    const res = await sut.execute(httpRequest);
+
+    // assert
+    expect(res.statusCode).toBe(200);
+  });
+});
