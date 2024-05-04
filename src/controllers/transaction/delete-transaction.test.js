a88e559636d941c57e29ba5b573414918237ec39
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-04 10:49:51 -0300

    test: ensure DeleteTransactionController returns 500 when DeleteTransacionUseCase throws

--- src/controllers/transaction/delete-transaction.test.js
+++ src/controllers/transaction/delete-transaction.test.js
@@ -62,4 +62,18 @@ describe("DeleteTransactionController", () => {
     // assert
     expect(res.statusCode).toBe(404);
   });
+
+  it("should return 500 when DeleteTransactionUseCase throws", async () => {
+    // arrange
+    const { sut, deleteTransactionUseCase } = makeSut();
+    jest
+      .spyOn(deleteTransactionUseCase, "execute")
+      .mockRejectedValueOnce(new Error());
+
+    // act
+    const res = await sut.execute(httpRequest);
+
+    // assert
+    expect(res.statusCode).toBe(500);
+  });
 });
