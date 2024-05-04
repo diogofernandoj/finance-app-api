f4447995ea3ede429e04e9f1e71f603c9e988d8c
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-04 12:03:04 -0300

    test: ensure UpdateTransactionController returns 400 when transaction id is not valid

--- src/controllers/transaction/update-transaction.test.js
+++ src/controllers/transaction/update-transaction.test.js
@@ -45,4 +45,15 @@ describe("UpdateTransactionController", () => {
     // assert
     expect(res.statusCode).toBe(200);
   });
+
+  it("should return 400 when transaction id is invalid", async () => {
+    // arrange
+    const { sut } = makeSut();
+
+    // act
+    const res = await sut.execute({ params: { transactionId: "invalid_id" } });
+
+    // assert
+    expect(res.statusCode).toBe(400);
+  });
 });
