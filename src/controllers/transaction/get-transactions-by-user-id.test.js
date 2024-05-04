7b28cd70c1436a130d84322584b791b14b8c7cf5
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-04 11:04:50 -0300

    test: ensure GetTransactionsByUserIdController returns 200 on getting transactions successfully

---
+++ src/controllers/transaction/get-transactions-by-user-id.test.js
@@ -0,0 +1,47 @@
+import { faker } from "@faker-js/faker";
+import { GetTransactionsByUserIdController } from "../index.js";
+import { TransactionType } from "@prisma/client";
+
+describe("GetTransactionsByUserId", () => {
+  class GetTransactionsByUserIdUseCaseStub {
+    async execute() {
+      return [
+        {
+          id: faker.string.uuid(),
+          user_id: faker.string.uuid(),
+          title: faker.string.alpha(10),
+          date: faker.date.anytime().toISOString(),
+          amount: faker.number.float(),
+          type: faker.helpers.enumValue(TransactionType),
+        },
+      ];
+    }
+  }
+
+  const makeSut = () => {
+    const getTransactionsByUserIdUseCase =
+      new GetTransactionsByUserIdUseCaseStub();
+    const sut = new GetTransactionsByUserIdController(
+      getTransactionsByUserIdUseCase,
+    );
+
+    return { sut, getTransactionsByUserIdUseCase };
+  };
+
+  const httpRequest = {
+    query: {
+      userId: faker.string.uuid(),
+    },
+  };
+
+  it("should return 200 on getting transactions successfully", async () => {
+    // arrange
+    const { sut } = makeSut();
+
+    // act
+    const res = await sut.execute(httpRequest);
+
+    // assert
+    expect(res.statusCode).toBe(200);
+  });
+});
