da1ba4d5f5612bbdee0ca361e7fda30757598363
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-04 16:18:35 -0300

    test: ensure CreateTransacionController calls CreateTransactionUseCase with correct params

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -154,4 +154,16 @@ describe("CreateTransactionController", () => {
     // assert
     expect(res.statusCode).toBe(500);
   });
+
+  it("should call CreateTransactionUseCase with correct params", async () => {
+    // arrange
+    const { sut, createTransactionUseCase } = makeSut();
+    const executeSpy = jest.spyOn(createTransactionUseCase, "execute");
+
+    // act
+    await sut.execute(httpRequest);
+
+    // assert
+    expect(executeSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
 });
