6fd9fc1907b5eefef6fc2aa45186e3a8524c5a7e
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-05 11:36:56 -0300

    refactor: remove unused code

--- src/controllers/helpers/user.js
+++ src/controllers/helpers/user.js
@@ -1,10 +1,4 @@
-import { badRequest, notFound } from "./http.js";
-
-export const invalidPasswordResponse = () =>
-  badRequest({ errorMessage: "Password must be at least 6 characters" });
-
-export const invalidEmailResponse = () =>
-  badRequest({ errorMessage: "The provided e-mail is not valid" });
+import { notFound } from "./http.js";
 
 export const userNotFoundResponse = () =>
   notFound({ errorMessage: "User not found" });
--- src/controllers/helpers/validation.js
+++ src/controllers/helpers/validation.js
@@ -5,26 +5,3 @@ export const checkIfIdIsValid = (id) => validator.isUUID(id);
 
 export const invalidIdResponse = () =>
   badRequest({ errorMessage: "The provided id is not valid" });
-
-export const requiredFieldIsMissingResponse = (field) => {
-  return badRequest({ errorMessage: `The field ${field} is required` });
-};
-
-const checkIfIsString = (value) => typeof value === "string";
-
-export const validateRequiredFields = (params, requiredFields) => {
-  for (const field of requiredFields) {
-    const fieldIsMissing = !params[field];
-    const fieldIsEmpty =
-      checkIfIsString(params[field]) &&
-      validator.isEmpty(params[field], { ignore_whitespace: true });
-    if (fieldIsMissing || fieldIsEmpty) {
-      return {
-        missingField: field,
-      };
-    }
-  }
-  return {
-    missingField: null,
-  };
-};
