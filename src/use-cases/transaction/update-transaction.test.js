61ad735e043ed3e10d69a937d342d08bf1c7d7ed
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-13 20:54:04 -0300

    test: ensure UpdateTransactionUseCase updates a transaction successfully

---
+++ src/use-cases/transaction/update-transaction.test.js
@@ -0,0 +1,46 @@
+import { faker } from "@faker-js/faker";
+import { UpdateTransactionUseCase } from "../index.js";
+import { TransactionType } from "@prisma/client";
+
+describe("UpdateTransactionUseCase", () => {
+  const transaction = {
+    id: faker.string.uuid(),
+    user_id: faker.string.uuid(),
+    title: faker.string.alpha(10),
+    date: faker.date.anytime().toISOString(),
+    type: faker.helpers.enumValue(TransactionType),
+    amount: faker.number.float(),
+  };
+
+  class UpdateTransactionRepositoryStub {
+    async execute(transactionId) {
+      return {
+        id: transactionId,
+        ...transaction,
+      };
+    }
+  }
+
+  const makeSut = () => {
+    const updateTransactionRepository = new UpdateTransactionRepositoryStub();
+    const sut = new UpdateTransactionUseCase(updateTransactionRepository);
+
+    return { sut, updateTransactionRepository };
+  };
+
+  it("should update a transaction successfully", async () => {
+    // arrange
+    const { sut } = makeSut();
+
+    // act
+    const res = await sut.execute(transaction.id, {
+      title: faker.string.alpha(10),
+      date: faker.date.anytime().toISOString(),
+      type: faker.helpers.enumValue(TransactionType),
+      amount: faker.number.float(),
+    });
+
+    // assert
+    expect(res).toEqual(transaction);
+  });
+});
