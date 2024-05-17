e8f627a57e24cc7ce5a3d384550437b5cbbbb571
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-16 21:24:39 -0300

    test: ensure PostgresGetUserBalanceRepository gets user balance from db

---
+++ src/repositories/postgres/user/get-user-balance.test.js
@@ -0,0 +1,66 @@
+import { faker } from "@faker-js/faker";
+import { prisma } from "../../../../prisma/prisma.js";
+import { user as fakeUser } from "../../../tests/index.js";
+import { PostgresGetUserBalanceRepository } from "../index.js";
+
+describe("PostgresGetUserBalanceRepository", () => {
+  const sut = new PostgresGetUserBalanceRepository();
+
+  it("should get user balance on db", async () => {
+    const user = await prisma.user.create({ data: fakeUser });
+
+    await prisma.transaction.createMany({
+      data: [
+        {
+          title: faker.string.alpha(10),
+          amount: 5000,
+          date: faker.date.recent().toISOString(),
+          type: "EARNING",
+          user_id: user.id,
+        },
+        {
+          title: faker.string.alpha(10),
+          date: faker.date.recent().toISOString(),
+          amount: 5000,
+          type: "EARNING",
+          user_id: user.id,
+        },
+        {
+          title: faker.string.alpha(10),
+          date: faker.date.recent().toISOString(),
+          amount: 1000,
+          type: "EXPENSE",
+          user_id: user.id,
+        },
+        {
+          title: faker.string.alpha(10),
+          date: faker.date.recent().toISOString(),
+          amount: 1000,
+          type: "EXPENSE",
+          user_id: user.id,
+        },
+        {
+          title: faker.string.alpha(10),
+          date: faker.date.recent().toISOString(),
+          amount: 3000,
+          type: "INVESTMENT",
+          user_id: user.id,
+        },
+        {
+          title: faker.string.alpha(10),
+          date: faker.date.recent().toISOString(),
+          amount: 3000,
+          type: "INVESTMENT",
+          user_id: user.id,
+        },
+      ],
+    });
+
+    const res = await sut.execute(user.id);
+
+    expect(res.earnings.toString()).toBe("10000");
+    expect(res.expenses.toString()).toBe("2000");
+    expect(res.investments.toString()).toBe("6000");
+    expect(res.balance.toString()).toBe("2000");
+  });
+});
