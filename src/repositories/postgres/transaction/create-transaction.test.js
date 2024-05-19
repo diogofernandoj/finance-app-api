12045b5e96200b17ce55a38abb636fae87adcad9
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-19 20:09:06 -0300

    test: ensure PostgresCreateTransactionRepository calls Prisma with correct params

--- src/repositories/postgres/transaction/create-transaction.test.js
+++ src/repositories/postgres/transaction/create-transaction.test.js
@@ -4,20 +4,35 @@ import { prisma } from "../../../../prisma/prisma";
 import dayjs from "dayjs";
 
 describe("PostgresCreateTransactionRepository", () => {
-  const sut = new PostgresCreateTransactionRepository();
   it("should create a transaction on db", async () => {
+    const sut = new PostgresCreateTransactionRepository();
     await prisma.user.create({ data: user });
 
-    const result = await sut.execute({ ...transaction, user_id: user.id });
+    const res = await sut.execute({ ...transaction, user_id: user.id });
 
-    expect(result.name).toBe(transaction.name);
-    expect(result.type).toBe(transaction.type);
-    expect(result.user_id).toBe(user.id);
-    expect(String(result.amount)).toBe(String(transaction.amount));
-    expect(dayjs(result.date).daysInMonth()).toBe(
+    expect(res.name).toBe(transaction.name);
+    expect(res.type).toBe(transaction.type);
+    expect(res.user_id).toBe(user.id);
+    expect(String(res.amount)).toBe(String(transaction.amount));
+    expect(dayjs(res.date).daysInMonth()).toBe(
       dayjs(transaction.date).daysInMonth(),
     );
-    expect(dayjs(result.date).month()).toBe(dayjs(transaction.date).month());
-    expect(dayjs(result.date).year()).toBe(dayjs(transaction.date).year());
+    expect(dayjs(res.date).month()).toBe(dayjs(transaction.date).month());
+    expect(dayjs(res.date).year()).toBe(dayjs(transaction.date).year());
+  });
+
+  it("should call Prisma with correct params", async () => {
+    await prisma.user.create({ data: user });
+    const sut = new PostgresCreateTransactionRepository();
+    const prismaSpy = jest.spyOn(prisma.transaction, "create");
+
+    await sut.execute({ ...transaction, user_id: user.id });
+
+    expect(prismaSpy).toHaveBeenCalledWith({
+      data: {
+        ...transaction,
+        user_id: user.id,
+      },
+    });
   });
 });
