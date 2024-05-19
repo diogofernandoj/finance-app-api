e71f1520070ca7344dc107e34b5a113c88d08b1f
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-19 18:10:06 -0300

    fix: ensure PostgresUpdateTransactionRepository uses data params correctly

--- src/repositories/postgres/transaction/update-transaction.js
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -6,9 +6,7 @@ export class PostgresUpdateTransactionRepository {
       where: {
         id: transactionId,
       },
-      data: {
-        updateTransactionParams,
-      },
+      data: updateTransactionParams,
     });
   }
 }
