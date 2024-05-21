55131c287db6672d9c18495062782b5f779172f5
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-21 20:48:45 -0300

    refactor: move user routes to separate file

--- index.js
+++ index.js
@@ -2,64 +2,19 @@ import "dotenv/config.js";
 import express from "express";
 
 import {
-  makeCreateUserController,
-  makeDeleteUserController,
-  makeGetUserBalanceController,
-  makeGetUserByIdController,
-  makeUpdateUserController,
-} from "./src/factories/controllers/user.js";
-
-import {
   makeCreateTransactionController,
   makeDeleteTransactionController,
   makeGetTransactionsByUserIdController,
   makeUpdateTransactionController,
 } from "./src/factories/controllers/transaction.js";
 
+import { usersRouter } from "./src/routes/user.js";
+
 const app = express();
 
 app.use(express.json());
 
-// USER ROUTES
-app.post("/api/users", async (request, response) => {
-  const createUserController = makeCreateUserController();
-
-  const { statusCode, body } = await createUserController.execute(request);
-
-  response.status(statusCode).send(body);
-});
-
-app.get("/api/users/:userId", async (request, response) => {
-  const getUserByIdController = makeGetUserByIdController();
-
-  const { statusCode, body } = await getUserByIdController.execute(request);
-
-  response.status(statusCode).send(body);
-});
-
-app.get("/api/users/:userId/balance", async (request, response) => {
-  const getUserBalance = makeGetUserBalanceController();
-
-  const { statusCode, body } = await getUserBalance.execute(request);
-
-  response.status(statusCode).send(body);
-});
-
-app.patch("/api/users/:userId", async (request, response) => {
-  const updateUserController = makeUpdateUserController();
-
-  const { statusCode, body } = await updateUserController.execute(request);
-
-  response.status(statusCode).send(body);
-});
-
-app.delete("/api/users/:userId", async (request, response) => {
-  const deleteUserController = makeDeleteUserController();
-
-  const { statusCode, body } = await deleteUserController.execute(request);
-
-  response.status(statusCode).send(body);
-});
+app.use("/api/users", usersRouter);
 
 // TRANSACTION ROUTES
 app.post("/api/transactions", async (request, response) => {
