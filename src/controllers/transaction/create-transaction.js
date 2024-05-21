452e3b3df51adc3d449582113be256eaf675762a
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-21 20:59:06 -0300

    feat: ensure UserNotFoundError is handled by CreateTransactionController

--- src/controllers/transaction/create-transaction.js
+++ src/controllers/transaction/create-transaction.js
@@ -1,6 +1,12 @@
 import { ZodError } from "zod";
 import { createTransactionSchema } from "../../schemas/index.js";
-import { serverError, created, badRequest } from "../helpers/index.js";
+import {
+  serverError,
+  created,
+  badRequest,
+  userNotFoundResponse,
+} from "../helpers/index.js";
+import { UserNotFoundError } from "../../errors/user.js";
 
 export class CreateTransactionController {
   constructor(createTransactionUseCase) {
@@ -20,6 +26,10 @@ export class CreateTransactionController {
     } catch (error) {
       if (error instanceof ZodError) {
         return badRequest({ message: error.errors[0].message });
+      }
+
+      if (error instanceof UserNotFoundError) {
+        return userNotFoundResponse();
       }
       console.error(error);
       return serverError();
