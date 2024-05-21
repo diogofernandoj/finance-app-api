028396d8194cfdb0a246068334c3122a65b7630c
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-21 20:02:08 -0300

    test: ensure makeUpdateTransactionController returns valid UpdateTransactionController

--- src/factories/controllers/transaction.test.js
+++ src/factories/controllers/transaction.test.js
@@ -1,10 +1,22 @@
-import { CreateTransactionController } from "../../controllers/index.js";
-import { makeCreateTransactionController } from "./transaction.js";
+import {
+  CreateTransactionController,
+  UpdateTransactionController,
+} from "../../controllers/index.js";
+import {
+  makeCreateTransactionController,
+  makeUpdateTransactionController,
+} from "./transaction.js";
 
 describe("Transaction Controller Factories", () => {
   it("should return a valid CreateTransactionController instance", () => {
     expect(makeCreateTransactionController()).toBeInstanceOf(
       CreateTransactionController,
+    );
+  });
+
+  it("should return a valid UpdateTransactionController instance", () => {
+    expect(makeUpdateTransactionController()).toBeInstanceOf(
+      UpdateTransactionController,
     );
   });
 });
