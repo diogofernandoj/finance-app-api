fd61f402664b1c6a91e7cf35ec026b94f1bb803a
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-22 20:09:01 -0300

    test: ensure PATCH /api/users/:userId returns 200 when user is updated

--- src/routes/user.e2e.test.js
+++ src/routes/user.e2e.test.js
@@ -2,6 +2,7 @@ import request from "supertest";
 
 import { app } from "../app.js";
 import { user } from "../tests/index.js";
+import { faker } from "@faker-js/faker";
 
 describe("User Routes E2E Tests", () => {
   it("POST /api/users should return 201 when user is created", async () => {
@@ -19,5 +20,31 @@ describe("User Routes E2E Tests", () => {
 
     expect(res.status).toBe(200);
     expect(res.body).toEqual(createdUser);
+  });
+
+  it("PATCH /api/users/:userId should return 200 when user is updated", async () => {
+    const { body: createdUser } = await request(app)
+      .post("/api/users")
+      .send({
+        ...user,
+        id: undefined,
+      });
+
+    const updateUserParams = {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    const res = await request(app)
+      .patch(`/api/users/${createdUser.id}`)
+      .send(updateUserParams);
+
+    expect(res.status).toBe(200);
+    expect(res.body.firstName).toBe(updateUserParams.firstName);
+    expect(res.body.lastName).toBe(updateUserParams.lastName);
+    expect(res.body.email).toBe(updateUserParams.email);
+    expect(res.body.password).not.toBe(createdUser.password);
   });
 });
